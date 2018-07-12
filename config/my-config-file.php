90560550a73ba318571c470a85263ebc4940ed7c
zhangjun <5024064@qq.com>
2018-07-12 15:46:28 +0800

    使用php-di/slim-bridge 1.0.2, 发现路由会无法识别name，已经向官方反应。

--- config/my-config-file.php
+++ config/my-config-file.php
@@ -5,27 +5,30 @@
  * Date: 2018/7/12
  * Time: 11:16 AM
  */
-use Slim\App;
 use Slim\Views\Twig;
 use function DI\get;
 use App\Model\Products;
+use Slim\Views\TwigExtension;
+use Interop\Container\ContainerInterface;
 
 return [
     'settings.displayErrorDetails' => true,
 
-    Twig::class => function (App $app) {//https://github.com/slimphp/Twig-View and http://php-di.org/doc/frameworks/slim.html
-        $c = $app->getContainer();
-        $twig = new \Slim\Views\Twig(__DIR__.'/../src/Template', [
-            'cache' => false//'path/to/cache'
+    'router' => get(Slim\Router::class),
+
+    Twig::class => function(ContainerInterface $c){
+        $twig = new Twig(__DIR__.'/../src/Template', [
+            'cache' => false
         ]);
 
-        $twig->addExtension(new \Slim\Views\TwigExtension(
+        $twig->addExtension(new TwigExtension(
             $c->get('router'),
             $c->get('request')->getUri()
         ));
 
         return $twig;
     },
+
 
     Products::class => function(){
         return new Products();
